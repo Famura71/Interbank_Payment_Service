2681986108c7667a225693dd8cdadf4313a9df6d
Tuna-Telatar <tunatelatar@gmail.com>
2025-11-29 17:02:10 +0300

    DAO class for User

---
+++ src/main/java/Database/DAO/UserDao.java
@@ -0,0 +1,58 @@
+package Database.DAO;
+
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import Database.Entities.User;
+
+@Repository        // Spring'e bu sınıfın bir DAO/repository olduğunu söylüyor
+@Transactional     // Her metotta otomatik transaction aç/kapa
+public class UserDao {
+
+    @Autowired
+    private SessionFactory sessionFactory;
+
+    private Session getSession() {
+        return sessionFactory.getCurrentSession();
+    }
+
+    // CREATE (insert)
+    public void save(User user) {
+        getSession().persist(user);
+    }
+
+    // UPDATE
+    public void update(User user) {
+        getSession().merge(user);
+    }
+
+    // DELETE
+    public void delete(User user) {
+        getSession().remove(user);
+    }
+
+    // READ (by ID)
+    public User getById(Long id) {
+        return getSession().find(User.class, id);
+    }
+
+    // READ (all)
+    public List<User> getAll() {
+        return getSession()
+                .createQuery("FROM User", User.class)
+                .getResultList();
+    }
+
+    // READ (by email)
+    public User getByEmail(String email) {
+        return getSession()
+                .createQuery("FROM User WHERE email = :email", User.class)
+                .setParameter("email", email)
+                .uniqueResult();
+    }
+}
